9a65bb6f01c5fe082cca2748a3b929793b66d0db
Zachary Lynch <zachary.lynch1337@gmail.com>
2020-08-09 19:45:27 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,27 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    "Hello world!"
   end
+  
+  get '/articles' do 
+    @articles = Article.all 
+    erb :index
+  end
+  
+  get '/articles/:id' do 
+    @article = Article.all.select do |article|
+      article.id = params[:id]
+    end.first
+    erb :show
+  end
+  
+  get '/articles/new' do 
+    
+    erb :new
+  end
+  
+  
+  
+  
 end
